b1567358b6efb46526fd2258345a11d8a7b45707
zachup100 <zachup100@users.noreply.github.com>
2020-12-18 22:07:49 -0500

    Added sub-commands to voice command

--- main.lua
+++ main.lua
@@ -10,11 +10,15 @@ local Members = {}
 function Parties.get(Id) return Parties[Id] end
 function Parties.new(Member, Category, Title)
   local Channel = Category:createVoiceChannel(Title)
+  local Permissions = Channel:getPermissionOverwriteFor(Member)
+  Permissions:clearAllPermissions()
+  Permissions:allowPermissions(Enum.permission.connect, Enum.permission.speak, Enum.permission.connect)
 
   local self = {}
   self.Owner = Member.id
   self.Access = {}
   self.Title = Title
+  self.Guild = Category.guild.id
   self.Id = Channel.id
 
   function self:GetChannel() return self.Id end
@@ -22,6 +26,63 @@ function Parties.new(Member, Category, Title)
     local Id = self.Id
     for Index, Value in pairs(self) do self[Index] = nil end
     Parties[Id] = nil
+  end
+  function self:HasAccess(Id)
+    if #self.Access == 0 then return false end
+    for int=1,#self.Access do
+      if self.Access[int] == Id then
+        return int
+      end
+    end
+    return false
+  end
+  function self:Permit(Id)
+    if self:HasAccess(Id) then return false, "0" end
+    local Guild = Bot:getGuild(self.Guild)
+    local Channel = Guild:getChannel(self.Id)
+    if not Channel then return false, "1" end
+    local Member = Guild:getMember(Id)
+    if not Member then return false, "2" end
+    local Permissions = Channel:getPermissionOverwriteFor(Member)
+    Permissions:clearAllPermissions()
+    Permissions:allowPermissions(Enum.permission.connect, Enum.permission.speak, Enum.permission.connect)
+    table.insert(self.Access, Id)
+    return true
+  end
+  function self:Revoke(Id)
+    if #self.Access == 0 then return false, "1" end
+    local Access = self:HasAccess(Id)
+    print(Access)
+    if not Access then return false, "2" end
+    local Guild = Bot:getGuild(self.Guild)
+    local Channel = Guild:getChannel(self.Id)
+    if not Channel then return false, "3" end
+    local Member = Guild:getMember(Id)
+    if not Member then return false, "4" end
+    local Permissions = Channel:getPermissionOverwriteFor(Member)
+    Permissions:clearAllPermissions()
+    Permissions:delete()
+    table.remove(self.Access, Access)
+    return true
+  end
+  function self:Lock()
+    local Guild = Bot:getGuild(self.Guild)
+    local Channel = Guild:getChannel(self.Id)
+    if not Channel then return false, "1" end
+    local Role = GetEveryoneRole(Guild)
+    local Permissions = Channel:getPermissionOverwriteFor(Role)
+    Permissions:clearAllPermissions()
+    Permissions:denyPermissions(Enum.permission.connect, Enum.permission.speak, Enum.permission.connect)
+    return true
+  end
+  function self:Unlock()
+    local Guild = Bot:getGuild(self.Guild)
+    local Channel = Guild:getChannel(self.Id)
+    if not Channel then return false, "1" end
+    local Role = GetEveryoneRole(Guild)
+    local Permissions = Channel:getPermissionOverwriteFor(Role)
+    Permissions:clearAllPermissions()
+    return true
   end
 
   Parties[Channel.id] = self
@@ -40,6 +101,22 @@ function Members.new(Member)
 
   Members[Member.id] = self
   return self
+end
+
+function GetEveryoneRole(Guild)
+  for _, Role in pairs(Guild.roles) do
+    if Role.name == "@everyone" then
+      return Role
+    end
+  end
+end
+
+function HasParty(Id)
+  local _Member = Members.get(Id)
+  if not _Member then return false end
+  local Party = Parties.get(_Member.Channel)
+  if not Party then return false end
+  return true, _Member, Party
 end
 
 local function split(input, sep)
@@ -122,13 +199,8 @@ function VoiceCommand(Member, Message, Args)
   elseif (Args[1] == "name" or Args[1] == "rename" or Args[1] == "title") then
     table.remove(Args, 1)
     local Title = table.concat(Args, " ")
-    local _Member = Members.get(Member.id)
-    if not _Member then
-      Message:reply(string.format("%s You do not own a voice channel!", Message.author.mentionString))
-      return
-    end
-    local Party = Parties.get(_Member.Channel)
-    if not Party then
+    local Exists, _Member, Party = HasParty(Member.id)
+    if not Exists then
       Message:reply(string.format("%s You do not own a voice channel!", Message.author.mentionString))
       return
     end
@@ -143,17 +215,121 @@ function VoiceCommand(Member, Message, Args)
     Message:reply("This sub-command is unfinished")
     return
   elseif Args[1] == "lock" then
-    Message:reply("This sub-command is unfinished")
+    local Exists, _Member, Party = HasParty(Member.id)
+    if not Exists then
+      Message:reply(string.format("%s You do not own a voice channel!", Message.author.mentionString))
+      return
+    end
+    local Success = Party:Lock()
+    if Success then
+      Message:reply("Your channel was locked!")
+    else
+      Message:reply("Error: unable to lock your channel!")
+    end
     return
   elseif Args[1] == "unlock" then
-    Message:reply("This sub-command is unfinished")
+    local Exists, _Member, Party = HasParty(Member.id)
+    if not Exists then
+      Message:reply(string.format("%s You do not own a voice channel!", Message.author.mentionString))
+      return
+    end
+    local Success = Party:Unlock()
+    if Success then
+      Message:reply("Your channel was unlocked!")
+    else
+      Message:reply("Error: unable to unlock your channel!")
+    end
     return
   elseif (Args[1] == "permit" or Args[1] == "allow") then
-    Message:reply("This sub-command is unfinished")
-    return
+    local Exists, _Member, Party = HasParty(Member.id)
+    if not Exists then
+      Message:reply(string.format("%s You do not own a voice channel!", Message.author.mentionString))
+      return
+    end
+    local Mentions = Message.mentionedUsers or {}
+    if #Mentions == 0 then Message:reply(string.format("%s You didn't mention anybody!", Message.author.mentionString)) return end
+    local Permitted = 0
+    if #Mentions == 1 then
+      local getMember = Message.guild:getMember(Mentions[1][1])
+      if not getMember then
+        Message:reply(string.format("%s That user isn't in this guild", Message.author.mentionString))
+        return
+      end
+      local User = getMember.user
+      if User.bot then
+        Message:reply(string.format("%s You cannot give access to Discord Bots", Message.author.mentionString))
+        return
+      end
+      if User.id == Member.id then
+        Message:reply(string.format("%s You already have access to your own channel!", Message.author.mentionString))
+        return
+      end
+      local Success, Output = Party:Permit(User.id)
+      if Success then
+        Message:reply(string.format("%s You gave access to '%s'", Message.author.mentionString, User.name))
+      else
+        Message:reply(string.format("%s Failed to give access. Error-Id %s", Message.author.mentionString, Output))
+      end
+      return
+    else
+      for _, Mention in pairs(Mentions) do
+        local getMember = Message.guild:getMember(Mention.id)
+        if getMember then
+          local User = getMember.user
+          if not User.bot and User.id ~= Member.id and not Party:HasAccess(User.id) then
+            Party:Permit(User.id)
+            Permitted = Permitted + 1
+          end
+        end
+      end
+      Message:reply(string.format("%s Permitted %s members.", Message.author.mentionString, Permitted))
+      return
+    end
   elseif (Args[1] == "revoke" or Args[1] == "remove") then
-    Message:reply("This sub-command is unfinished")
-    return
+    local Exists, _Member, Party = HasParty(Member.id)
+    if not Exists then
+      Message:reply(string.format("%s You do not own a voice channel!", Message.author.mentionString))
+      return
+    end
+    local Mentions = Message.mentionedUsers or {}
+    if #Mentions == 0 then Message:reply(string.format("%s You didn't mention anybody!", Message.author.mentionString)) return end
+    local Revoked = 0
+    if #Mentions == 1 then
+      local getMember = Message.guild:getMember(Mentions[1][1])
+      if not getMember then
+        Message:reply(string.format("%s That user isn't in this guild", Message.author.mentionString))
+        return
+      end
+      local User = getMember.user
+      if User.id == Member.id then
+        Message:reply(string.format("%s You can't remove access to your own channel!", Message.author.mentionString))
+        return
+      end
+      local Success, Output = Party:Revoke(User.id)
+      if Success then
+        Message:reply(string.format("%s You removed access from '%s'", Message.author.mentionString, User.name))
+      else
+        if Output == "1" or Output == "2" then
+          Message:reply(string.format("%s That member doesn't have access to your channel.", Message.author.mentionString, Output))
+        else
+          Message:reply(string.format("%s Failed to remove access. Error-Id %s", Message.author.mentionString, Output))
+        end
+      end
+      return
+    else
+      for _, Mention in pairs(Mentions) do
+        local getMember = Message.guild:getMember(Mention.id)
+        if getMember then
+          local User = getMember.user
+          if User.id ~= Member.id and Party:HasAccess(User.id) then
+            Party:Revoke(User.id)
+            Revoked = Revoked + 1
+          end
+        end
+      end
+      Message:reply(string.format("%s Removed %s members.", Message.author.mentionString, Permitted))
+      return
+    end
   end
 end
 
@@ -223,6 +399,7 @@ function MemberJoinedVoice(Member, Channel)
   if not Party then
     local Category = Member.guild:getChannel(Guild.Party.Category)
     if Category then
+      print("New Party event")
       Party = Parties.new(Member, Category, string.gsub(Guild.Party.Template, "{USERNAME}", Member.name))
       local VoiceId = Party:GetChannel()
       _Member:SetChannel(VoiceId)
@@ -236,6 +413,7 @@ function MemberJoinedVoiceSafe(Member, Channel)
   if not Success then print("[MemberJoinedVoice]:",Result) end
 end
 
+--// Fix this, doesn't always delete channel
 function MemberLeftVoice(Member, Channel)
   local Party = Parties.get(Channel.id)
   if Party then
